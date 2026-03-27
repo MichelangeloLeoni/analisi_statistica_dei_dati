f00a14f977465bc2a9e99971a409f39a98eb1707
MichelangeloLeoni <leonimichelangelo17@gmail.com>
2026-03-27 14:25:15 +0100

    - Add src folder to host py scripts genereting images  - Add chebyshev theorem dim plot  -Update .gitignore

---
+++ src/chebyshev_theorem.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use("pgf")
+matplotlib.rcParams.update({
+    "pgf.texsystem": "pdflatex",
+    "text.usetex": True,
+    "pgf.rcfonts": False,
+    "font.family": "serif", 
+    "font.size": 10,  
+    "pgf.preamble": r"""
+        \usepackage{amsmath}
+        \usepackage{mathrsfs}
+    """
+})
+
+mu = 3
+sigma = 1.2
+k = 1.5
+y_val = k * sigma
+xx = np.linspace(mu - 4, mu + 4, 1000)
+
+def indicator_function(x, mu, threshold):
+    return np.where(np.abs(x - mu) >= threshold, 1.0, 0.0)
+
+fig, ax = plt.subplots(figsize=(5.5, 3.5))
+
+ax.hlines(y_val, xx.min(), xx.max(), linestyles=":", color='gray', alpha=0.5, label=r'$k\sigma$')
+
+ax.plot(xx, np.abs(xx - mu), label=r'$|x - \mu|$', color='#1f77b4', lw=1.5, linestyle='--')
+
+y_ind = indicator_function(xx, mu, y_val)
+ax.plot(xx, y_ind, label=r'$ \mathscr{F} (|x - \mu| \geq k\sigma)$', color='#d62728', linewidth=2)
+
+ax.set_xticks([mu, mu - y_val, mu + y_val])
+ax.set_xticklabels([r'$\mu$', r'$\mu-k\sigma$', r'$\mu+k\sigma$'])
+ax.set_yticks([0, 1, y_val])
+ax.set_yticklabels(['0', '1', r'$k\sigma$'])
+
+ax.legend(loc='upper right', fontsize='small')
+ax.grid(True, linestyle=':', alpha=0.3)
+
+plt.savefig("images/chebyshev_theorem.pgf", bbox_inches='tight')
+plt.close()
