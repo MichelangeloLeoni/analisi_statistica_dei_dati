b8dd67aeac15f2db561ad6edf75b51d952e37cec
MichelangeloLeoni <leonimichelangelo17@gmail.com>
2026-04-23 12:55:44 +0200

    mior

--- src/lr_ordering.py
+++ src/lr_ordering.py
@@ -72,22 +72,23 @@ ax1.plot(x, pdf, lw=2, label=r"$\mathcal{N}(x|\mu)$")
 ax1.fill_between(x, 0, pdf, where=mask, alpha=0.35)
 
 ax1.set_xlabel("x")
+ax1.set_xlim(x[0], x[-1])
 ax1.set_ylabel("pdf")
 ax1.set_ylim(0, max(pdf)*1.2)
 
 # second axis for LR
 ax2 = ax1.twinx()
 trasl = 0.5 # shift LR up for better visibility
-ax2.plot(x, R+trasl, lw=2, ls="--", label=r"$LR(x)$")
-ax2.axhline(c+trasl, ls=":", color="black", lw=1.0, label="Threshold")
+ax2.plot(x, R, lw=2, ls="--", label=r"$LR(x)$")
+ax2.axhline(c, ls=":", color="black", lw=1.0, label="Threshold")
 
 # Vertical dotted lines at intersections
 for idx in starts + ends:
     x_inter = x[idx]
-    ax2.vlines(x_inter, 0, c + trasl, ls=":", color="black", lw=1.0)
+    ax2.vlines(x_inter, -trasl, c, ls=":", color="black", lw=1.0)
 
-ax2.set_ylabel("LR ratio + 0.5")
-ax2.set_ylim(0, max(R+trasl)*1.1)
+ax2.set_ylabel("LR ratio")
+ax2.set_ylim(-trasl, max(R)*1.1)
 # cosmetics
 ax1.set_title(r"Likelihood-ratio ordering for $\mu=0.5$")
 ax1.grid(alpha=0.25)
