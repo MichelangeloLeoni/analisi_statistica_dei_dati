e889e8ab928eb0ee8b87e9d526f64610ed41ba2e
MichelangeloLeoni <leonimichelangelo17@gmail.com>
2026-04-24 21:29:15 +0200

    Lint binomial_likelihood.py

--- scripts/binomial_likelihood.py
+++ scripts/binomial_likelihood.py
@@ -1,22 +1,28 @@
+'''Script that plots the binomial likelihood function for different values of n and k.'''
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.special import comb
 from asd import utils
 
-k1 = 1
-k2 = 0
-n = 1
-k3 = 25
+K1 = 1
+K2 = 0
+N = 1
+K3 = 25
 xx = np.linspace(0, 1, 1000)
 
 def binomial_pmf(k, n, p):
+    '''
+    Compute binomial probability mass function for k successes
+    in n trials given success probability p.
+    '''
+
     return comb(n, k) * (p ** k) * ((1 - p) ** (n - k))
 
 fig, ax = utils.pgf_generator(figsize=(5.5, 3.5))
 
-ax.plot(xx, binomial_pmf(k1, n, xx), label=r'$L_{n=1,k=1}$', color='#1f77b4', lw=1.5)
-ax.plot(xx, binomial_pmf(k2, n, xx), label=r'$L_{n=1,k=0}$', color='#ff7f0e', lw=1.5)
-ax.plot(xx, binomial_pmf(k3, 50*n, xx), label=r'$L_{n=50,k=25}$', color='#2ca02c', lw=1.5)
+ax.plot(xx, binomial_pmf(K1, N, xx), label=r'$L_{n=1,k=1}$', color='#1f77b4', lw=1.5)
+ax.plot(xx, binomial_pmf(K2, N, xx), label=r'$L_{n=1,k=0}$', color='#ff7f0e', lw=1.5)
+ax.plot(xx, binomial_pmf(K3, 50*N, xx), label=r'$L_{n=50,k=25}$', color='#2ca02c', lw=1.5)
 
 ax.set_xticks([0,0.5,1])
 ax.set_xticklabels([0,0.5,1])
