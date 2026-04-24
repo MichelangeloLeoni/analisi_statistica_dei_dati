ff8aa1f8ce0448c658952a46a69434cecad4a19d
MichelangeloLeoni <leonimichelangelo17@gmail.com>
2026-04-24 18:45:24 +0200

    Lint binomali_coverage.py

--- scripts/binomial_coverage.py
+++ scripts/binomial_coverage.py
@@ -1,20 +1,23 @@
+'''
+Script that generates two plots to illustrate the construction of Neyman confidence
+belts for a binomial model using different ordering methods, 
+and the corresponding coverage curves.
+'''
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import binom
 from asd import utils
 
-n = 20                 # sample size
-alpha = 0.05           # 95% confidence intervals
-CL = 1 - alpha
-
-p_grid = np.linspace(0, 1, 100)
-x_vals = np.arange(n + 1)
+# Define parameters
+N = 20
+ALPHA = 0.05
+CL = 1 - ALPHA
 
 # Acceptance rules for confidence belt construction
-def acceptance_p_ordering(p):
-    probs = binom.pmf(x_vals, n, p)
+def acceptance_p_ordering(p, x_vals, cl=CL):
+    '''Compute acceptance region following probability ordering.'''
 
-    # order x by decreasing probability P(x|p)
+    probs = binom.pmf(x_vals, N, p)
     order = np.argsort(probs)[::-1]
 
     total = 0.0
@@ -23,13 +26,15 @@ def acceptance_p_ordering(p):
     for i in order:
         acc.add(x_vals[i])
         total += probs[i]
-        if total >= CL:
+        if total >= cl:
             break
 
     return acc
 
-def acceptance_upper(p):
-    probs = binom.pmf(x_vals, n, p)
+def acceptance_upper(p, x_vals, cl=CL):
+    '''Compute acceptance region following upper ordering.'''
+
+    probs = binom.pmf(x_vals, N, p)
 
     total = 0
     acc = set()
@@ -37,13 +42,15 @@ def acceptance_upper(p):
     for x in x_vals:
         acc.add(x)
         total += probs[x]
-        if total >= CL:
+        if total >= cl:
             break
 
     return acc
 
-def acceptance_lower(p):
-    probs = binom.pmf(x_vals, n, p)
+def acceptance_lower(p, x_vals, cl=CL):
+    '''Compute acceptance region following lower ordering.'''
+
+    probs = binom.pmf(x_vals, N, p)
 
     total = 0
     acc = set()
@@ -51,69 +58,36 @@ def acceptance_lower(p):
     for x in reversed(x_vals):
         acc.add(x)
         total += probs[x]
-        if total >= CL:
+        if total >= cl:
             break
 
     return acc
 
+def build_belt(acceptance_func, p_grid, cl=CL):
+    '''Compute Neyman confidence belt based on giveng acceptance function.'''
 
-# Build confidence belt
-def build_belt(acceptance_func):
-    x_low = []
-    x_high = []
+    temp_x_low = []
+    temp_x_high = []
 
     for p in p_grid:
-        acc = sorted(list(acceptance_func(p)))
-        x_low.append(min(acc))
-        x_high.append(max(acc))
+        acc = sorted(list(acceptance_func(p, x_values, cl=cl)))
+        temp_x_low.append(min(acc))
+        temp_x_high.append(max(acc))
 
-    return np.array(x_low), np.array(x_high)
+    return np.array(temp_x_low), np.array(temp_x_high)
 
-fig, axes = utils.pgf_generator(nrows=1, ncols=3, figsize=(5.5, 3.5), sharey=True)
+def invert_belt(acceptance_func, x_vals, p_grid, cl=CL):
+    '''
+    Compute confidence intervals for a given observed x based on
+    giveng acceptance function and probability grid.
+    '''
 
-methods = [
-    ("Probability ordering", acceptance_p_ordering),
-    ("Upper ordering", acceptance_upper),
-    ("Lower ordering", acceptance_lower),
-
-]
-
-# Plot Neyman belts
-for ax, (title, func) in zip(axes, methods):
-
-    x_low, x_high = build_belt(func)
-
-    ax.fill_between(
-        p_grid,
-        x_low,
-        x_high,
-        alpha=0.4
-    )
-    ax.plot(p_grid, x_low, lw=1, color="black")
-    ax.plot(p_grid, x_high, lw=1, color="black")
-
-    ax.set_title(title)
-    ax.set_xlabel("p")
-    ax.set_xlim(0, 1)
-    ax.set_ylim(-0.5, n + 0.5)
-    ax.grid(alpha=0.25)
-
-axes[0].set_ylabel("x")
-
-plt.savefig("images/binomial_Neyman_belts.pgf", bbox_inches="tight")
-plt.close()
-
-# Build confidence intervals by inversion
-def invert_belt(acceptance_func):
-    """
-    For each observed x, confidence interval in p obtained by inversion.
-    """
     intervals = {}
     for x_obs in x_vals:
         good_p = []
 
         for p in p_grid:
-            acc = acceptance_func(p)
+            acc = acceptance_func(p, x_vals, cl=cl)
             if x_obs in acc:
                 good_p.append(p)
 
@@ -124,9 +98,12 @@ def invert_belt(acceptance_func):
 
     return intervals
 
+def coverage_curve(intervals, p_grid, x_vals):
+    '''
+    Compute coverage curve for given confidence intervals, 
+    probability grid and possible x values.
+    '''
 
-# Coverage calculator
-def coverage_curve(intervals):
     cov = []
 
     for p in p_grid:
@@ -135,40 +112,69 @@ def coverage_curve(intervals):
         for x in x_vals:
             lo, hi = intervals[x]
             if lo <= p <= hi:
-                total += binom.pmf(x, n, p)
+                total += binom.pmf(x, N, p)
 
         cov.append(total)
 
     return np.array(cov)
 
+prob_grid = np.linspace(0, 1, 100)
+x_values = np.arange(N + 1)
+methods = [
+    ("Probability ordering", acceptance_p_ordering),
+    ("Upper ordering", acceptance_upper),
+    ("Lower ordering", acceptance_lower)
+]
+int_p_ordering  = invert_belt(acceptance_p_ordering, x_values, prob_grid)
+int_upper       = invert_belt(acceptance_upper, x_values, prob_grid)
+int_lower       = invert_belt(acceptance_lower, x_values, prob_grid)
 
-# Compute intervals
-int_p_ordering  = invert_belt(acceptance_p_ordering)
-int_upper       = invert_belt(acceptance_upper)
-int_lower       = invert_belt(acceptance_lower)
-
-# Coverage curves
-cov_p_ordering  = coverage_curve(int_p_ordering)
-cov_upper       = coverage_curve(int_upper)
-cov_low         = coverage_curve(int_lower)
+cov_p_ordering  = coverage_curve(int_p_ordering, prob_grid, x_values)
+cov_upper       = coverage_curve(int_upper, prob_grid, x_values)
+cov_low         = coverage_curve(int_lower, prob_grid, x_values)
 
 titles = [
     "Probability ordering",
     "Upper ordering",
     "Lower ordering"
 ]
-
 curves = [
     cov_p_ordering,
     cov_upper,
     cov_low
 ]
 
-# Plot coverage curves
-fig, axes = plt.subplots(1, 3, figsize=(5.5, 3.5), sharey=True)
+# Generate first plot
+fig, axes = utils.pgf_generator(nrows=1, ncols=3, figsize=(5.5, 3.5), sharey=True)
+
+for ax, (title, func) in zip(axes, methods):
+    x_low, x_high = build_belt(func, prob_grid)
+
+    ax.fill_between(
+        prob_grid,
+        x_low,
+        x_high,
+        alpha=0.4
+    )
+    ax.plot(prob_grid, x_low, lw=1, color="black")
+    ax.plot(prob_grid, x_high, lw=1, color="black")
+
+    ax.set_title(title)
+    ax.set_xlabel("p")
+    ax.set_xlim(0, 1)
+    ax.set_ylim(-0.5, N + 0.5)
+    ax.grid(alpha=0.25)
+
+axes[0].set_ylabel("x")
+
+plt.savefig("images/binomial_Neyman_belts.pgf", bbox_inches="tight")
+plt.close()
+
+# Generate second plot
+fig, axes = utils.pgf_generator(nrows=1, ncols=3, figsize=(5.5, 3.5), sharey=True)
 
 for ax, title, curve in zip(axes, titles, curves):
-    ax.plot(p_grid, curve, lw=2)
+    ax.plot(prob_grid, curve, lw=2)
     ax.axhline(CL, ls="--", lw=1.2, alpha=0.8)
     ax.set_title(title)
     ax.set_xlabel("p")
@@ -176,7 +182,6 @@ for ax, title, curve in zip(axes, titles, curves):
     ax.set_ylim(CL-0.03, 1.01)
     ax.grid(alpha=0.25)
 
-# Shared y-label
 axes[0].set_ylabel("Coverage probability")
 
 plt.savefig("images/binomial_coverage.pgf", bbox_inches='tight')
