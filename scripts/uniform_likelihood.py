a229d212f5fe2db29d9f8e2b7ada2d4fa91c936a
MichelangeloLeoni <leonimichelangelo17@gmail.com>
2026-04-26 18:57:07 +0200

    Lint uniform_likelihood

--- scripts/uniform_likelihood.py
+++ scripts/uniform_likelihood.py
@@ -1,20 +1,25 @@
+'''
+Script that plots the likelihood function for a uniform distribution with unknown upper bound,
+given a single observation.
+'''
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.special import comb
 from asd import utils
 
-x0 = 1
+# Define parameters
+X0 = 1
 xx = np.linspace(0, 10, 1000)
 
 likelihood = np.zeros_like(xx)
-mask = (xx >= x0)
+mask = xx >= X0
 likelihood[mask] = 1 / xx[mask]
 
+# Generate plot
 fig, ax = utils.pgf_generator(figsize=(5.5, 3.5))
 
-ax.plot(xx, likelihood, label=r'$L_{x_0}(m)$', color='#1f77b4', lw=1.5)
+ax.plot(xx, likelihood, label=r'$L_{X_0}(m)$', color='#1f77b4', lw=1.5)
 
-ax.set_xticks([x0])
+ax.set_xticks([X0])
 ax.set_xticklabels([r'$x_0$'])
 ax.set_yticks([0,1])
 ax.set_yticklabels([0,1])
