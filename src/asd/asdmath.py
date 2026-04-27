4da2f823bb0046a5245334de03a9d62ef6a103a2
MichelangeloLeoni <leonimichelangelo17@gmail.com>
2026-04-27 11:27:17 +0200

    minor

--- src/asd/asdmath.py
+++ src/asd/asdmath.py
@@ -25,42 +25,44 @@ def find_intervals_indices(mask):
 
     return starts, ends
 
-def find_lr_intervals(x_obs, x_range, mu_grid, mu_hat, prob_func, cl):
-    '''
-    Compute the likelihood-ratio intervals at confidence level cl for a given observed value x_obs,
-    using the probability function prob_func and building the neyman belt for parameter values
-    mu_grid (could be used, for example, in poissonian or gaussian cases).
+def feldman_cousins_slice(x_range, mu, mu_hat_func, prob_func, cl, dx=None, discrete=True):
 
-    Parameters:
-        x_obs: the observed value for which to compute the interval
-        x_range: the range of possible observed values to consider when building the neyman belt
-        mu_grid: the range of parameter values to consider when building the neyman belt
-        mu_hat: the function that computes the best-fit parameter value for a given observed value
-        prob_func: the probability function that takes x_range and mu as 
-            arguments and returns the pdf values
-        cl: the confidence level for the interval
-    '''
+    # Compute Neyman belt slice
+    pdf = prob_func(x_range, mu)
+    den = prob_func(x_range, mu_hat_func(x_range))
+    r = pdf / den
 
-    # Initialize list to store mu values that generate intervals
-    # contatining n_obs at confidence level cl
+    order = np.argsort(r)[::-1]
+
+    cum = 0
+    for i in order:
+        if discrete:
+            cum += pdf[i]
+        else:
+            cum += pdf[i] * dx
+
+        if cum >= cl:
+            c = r[i]
+            break
+
+    mask = r >= c
+
+    return mask
+
+def lr_intervals(x_obs, x_range, mu_grid, mu_hat_func, prob_func, cl, dx=None, discrete=True):
+
     accepted_mu = []
 
-    # Compute Neyman belt
     for mu in mu_grid:
-        pdf = prob_func(x_range, mu)
-        den = prob_func(x_range, mu_hat)
-        r = pdf / den
-
-        order = np.argsort(r)[::-1]
-
-        cum = 0
-        for i in order:
-            cum += pdf[i]
-            if cum >= cl:
-                c = r[i]
-                break
-
-        mask = r >= c
+        mask = feldman_cousins_slice(
+            x_range=x_range,
+            mu=mu,
+            mu_hat_func=mu_hat_func,
+            prob_func=prob_func,
+            cl=cl,
+            dx=dx,
+            discrete=discrete
+        )
 
         if x_obs < len(mask) and mask[x_obs]:
             accepted_mu.append(mu)
